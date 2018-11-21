2f6ceff31630b5b78f5187229d45a3d3dd6167a9
RebeccaLassiter <32859765+RebeccaLassiter@users.noreply.github.com>
2018-11-20 19:01:00 -0600

    Home Page Initial Structure

--- app/src/main/java/com/example/rebecca/matchav11/MainActivity.java
+++ app/src/main/java/com/example/rebecca/matchav11/MainActivity.java
@@ -16,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
+    /*public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }*/
 }
